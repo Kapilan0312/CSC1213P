ca14155b114a85594f314eeb513e59ede3092fa3
Kapilan0312 <kapilan0312@gmail.com>
2025-03-14 23:11:51 +0530

    Add files via upload

---
+++ Ex4/Ex4.java
@@ -0,0 +1,16 @@
+class StuDetails {
+	public static void main(String [] args){
+		System.out.println("Name: Sabanayakam Kapilan");
+		System.out.println("Registration Number:2022/ASP/86");
+		System.out.println("Degree Program: Bsc.Applied Mathematics and Computing");
+		System.out.println();
+		
+		String Name="Sabanayakam Kapilan";
+		String RegNo="2022/ASP/86";
+		String Degree="Bsc.Applied Mathematics and Computing";
+		
+		System.out.println("Full Name: "+Name);
+		System.out.println("Registration Number: "+RegNo);
+		System.out.println("Degree Program: "+Degree);
+	}
+}
---
+++ Ex5/Ex5.java
@@ -0,0 +1,5 @@
+class StudentDetails{
+	public static void main (String[] args){
+		System.out.println("First Name: Sabanayakam \nLast Name: Kapilan \nAddress : Vaddukoddai,Jaffna");
+	}
+}
